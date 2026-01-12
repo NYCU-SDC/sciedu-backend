0733a65ec82d497f0279dbf521bef434e01d1f2c
asciibase64 <ilsao@outlook.com>
2026-01-12 15:51:59 +0800

    feat: add question create api

--- go.mod
+++ go.mod
@@ -4,19 +4,23 @@ go 1.25.1
 
 require (
 	github.com/NYCU-SDC/summer v1.0.0-test
-	github.com/golang-migrate/migrate/v4 v4.18.2
+	github.com/go-playground/validator/v10 v10.26.0
 	github.com/joho/godotenv v1.5.1
 	go.uber.org/zap v1.27.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
---
+++ internal/questions/service.go
@@ -0,0 +1,67 @@
+package questions
+
+import (
+	"context"
+	"strconv"
+
+	"go.uber.org/zap"
+)
+
+// test only, should not appear on actual product
+var testQuestionList []Question
+
+type Question struct {
+	ID      string
+	Type    string
+	Content string
+	Options []Option
+}
+
+type CreateParam struct {
+	Type       string
+	Content    string
+	ReqOptions []ReqOption
+}
+
+type Querier interface {
+	Create(ctx context.Context, arg CreateParam) (Question, error)
+}
+
+type Service struct {
+	logger *zap.Logger
+	// we don't need queries now
+	// queries Querier
+}
+
+// func NewService(logger *zap.Logger, queries Querier) *Service
+func NewService(logger *zap.Logger) *Service {
+	return &Service{
+		logger: logger,
+		// queries: queries,
+	}
+}
+
+func (s *Service) Create(ctx context.Context, arg CreateParam) (Question, error) {
+	// handle mock ID for each ReqOption
+	var options []Option
+	for i, option := range arg.ReqOptions {
+		options = append(options, Option{
+			ID:      strconv.Itoa(i),
+			Label:   option.Label,
+			Content: option.Content,
+		})
+	}
+
+	res := Question{
+		ID:      strconv.Itoa(len(testQuestionList)),
+		Type:    arg.Type,
+		Content: arg.Content,
+		Options: options,
+	}
+
+	testQuestionList = append(testQuestionList, res)
+
+	// here should have some error handle after connected the database...
+
+	return res, nil
+}
