ad80b05562c38aeaef19068d6b73f50ffc20aea5
asciibase64 <ilsao@outlook.com>
2026-01-13 12:21:45 +0800

    feat: be able to get and modify a question by id

--- internal/questions/service.go
+++ internal/questions/service.go
@@ -2,6 +2,7 @@ package questions
 
 import (
 	"context"
+	"errors"
 	"strconv"
 
 	"go.uber.org/zap"
@@ -17,14 +18,14 @@ type Question struct {
 	Options []Option
 }
 
-type CreateParam struct {
+type ReqParam struct {
 	Type       string
 	Content    string
 	ReqOptions []ReqOption
 }
 
 type Querier interface {
-	Create(ctx context.Context, arg CreateParam) (Question, error)
+	Create(ctx context.Context, arg ReqParam) (Question, error)
 }
 
 type Service struct {
@@ -41,8 +42,8 @@ func NewService(logger *zap.Logger) *Service {
 	}
 }
 
-func (s *Service) Create(ctx context.Context, arg CreateParam) (Question, error) {
-	// handle mock ID for each ReqOption
+func (s *Service) Create(ctx context.Context, arg ReqParam) (Question, error) {
+	// handle mock ID for each ReqOption, temporary...
 	var options []Option
 	for i, option := range arg.ReqOptions {
 		options = append(options, Option{
@@ -52,20 +53,74 @@ func (s *Service) Create(ctx context.Context, arg CreateParam) (Question, error)
 		})
 	}
 
-	res := Question{
+	question := Question{
 		ID:      strconv.Itoa(len(testQuestionList)),
 		Type:    arg.Type,
 		Content: arg.Content,
 		Options: options,
 	}
 
-	testQuestionList = append(testQuestionList, res)
+	testQuestionList = append(testQuestionList, question)
 
 	// here should have some error handle after connected the database...
 
-	return res, nil
+	return question, nil
 }
 
 func (s *Service) List(ctx context.Context) ([]Question, error) {
 	return testQuestionList, nil
+}
+
+func (s *Service) GetQuestion(ctx context.Context, ID string) (Question, error) {
+	// parse UUID logic, skip now...
+
+	// temporary string convert logic
+	questionID, err := strconv.Atoi(ID)
+	if err != nil {
+		s.logger.Error("failed to convert ID from string to int", zap.Error(err))
+		return Question{}, err
+	}
+
+	if questionID >= len(testQuestionList) {
+		s.logger.Error("id is not exists")
+		return Question{}, errors.New("id is not exists")
+	}
+
+	return testQuestionList[questionID], nil
+}
+
+func (s *Service) UpdateQuestion(ctx context.Context, ID string, arg ReqParam) (Question, error) {
+	// parse UUID logic, skip now...
+
+	// temporary string convert logic
+	questionID, err := strconv.Atoi(ID)
+	if err != nil {
+		s.logger.Error("failed to convert ID from string to int", zap.Error(err))
+		return Question{}, err
+	}
+
+	if questionID >= len(testQuestionList) {
+		s.logger.Error("id is not exists")
+		return Question{}, errors.New("id is not exists")
+	}
+
+	// handle mock ID for each ReqOption, temporary...
+	var options []Option
+	for i, option := range arg.ReqOptions {
+		options = append(options, Option{
+			ID:      strconv.Itoa(i),
+			Label:   option.Label,
+			Content: option.Content,
+		})
+	}
+
+	question := Question{
+		ID:      strconv.Itoa(questionID),
+		Type:    arg.Type,
+		Content: arg.Content,
+		Options: options,
+	}
+
+	testQuestionList[questionID] = question
+	return testQuestionList[questionID], err
 }
