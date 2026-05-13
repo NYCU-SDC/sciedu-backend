e83ba47d981909976866259a6c8ffae80542fed8
007d83QQ <julian.xd.8787@gmail.com>
2026-05-13 21:37:18 +0800

    feat: add users migration table with up and down scripts

---
+++ internal/database/migrations/7_users.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    avatar_url TEXT,
+    roles TEXT[] NOT NULL CHECK (ARRAY['STUDENT', 'EXPERIMENTER', 'ADMIN']),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
